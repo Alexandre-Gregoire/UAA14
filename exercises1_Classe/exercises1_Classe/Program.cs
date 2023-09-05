d9cbaa4140d145dc5d42d4fe18743de9145662fe
Grégoire Alexandre <210278@CCM-Asty-Moulin.local>
2023-09-05 14:28:10 +0200

    exercises sur les chiens

---
+++ exercises1_Classe/exercises1_Classe/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exercises1_Classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("generation de vos Victor");
+            Victor victor1 = new Victor("victooooooooor", "humain pas gentil", 12, true, true,"male",true,false);
+            Victor victor2 = new Victor("victaur", "humain vrm pas gentil", 14, true, true, "male", true, false);
+            Victor victor3 = new Victor("vyctor", "humain pas", 11, true, true, "male", true, false);
+
+            Console.WriteLine(victor1.AffichageVictor());
+            Console.WriteLine(victor2.AffichageVictor());
+            Console.WriteLine(victor3.AffichageVictor());
+        }
+    }
+}
---
+++ exercises1_Classe/exercises1_Classe/Victor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercises1_Classe
+{
+    class Victor
+    {
+        // attributs privés par convention, leur nom commence par '_'
+        private string _nom;
+        private string _race;
+        private int _age;
+        private bool _enOrdreDeVaccin;
+        private bool _puce;
+        private string _genre;
+        private bool _sterilise;
+        private bool _dresse;
+
+
+        public Victor(string nom, string race, int age, bool enOrdreDeVaccin, bool puce, string genre, bool sterilise, bool dresse)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+            _enOrdreDeVaccin = enOrdreDeVaccin;
+            _puce = puce;
+            _genre = genre;
+            _sterilise = sterilise;
+            _dresse = dresse;
+        }
+
+        public string AffichageVictor()
+        {
+            string chaine = "Nom : " + _nom + "// Race : " + _race + "Age : "+ _age +" // Vaccin : "+ _enOrdreDeVaccin +" // Puce : "+ _puce +" // Genre : "+ _genre +" // Sterilisé : "+ _sterilise +" // dresse : " + _dresse;
+            return chaine;
+        }
+        public void Promener() {
+            Console.WriteLine("Votre victor a été promener");
+        }
+        public void Vacciner()
+        {
+            Console.WriteLine("Votre victor a été vacciner");
+        }
+        public void Dresser()
+        {
+            Console.WriteLine("Votre victor a été dresser");
+        }
+    }
+}
