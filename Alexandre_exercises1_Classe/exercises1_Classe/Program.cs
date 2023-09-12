2ee913e21d1bb793eb016b771839fce69ce10343
Grégoire Alexandre <210278@CCM-Asty-Moulin.local>
2023-09-12 14:46:04 +0200

    finition du generateur de victor

--- Alexandre_exercises1_Classe/exercises1_Classe/Program.cs
+++ Alexandre_exercises1_Classe/exercises1_Classe/Program.cs
@@ -1,17 +1,101 @@
-﻿namespace exercises1_Classe
+﻿using System.Diagnostics;
+
+namespace exercises1_Classe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("generation de vos Victor");
-            Victor victor1 = new Victor("victooooooooor", "humain pas gentil", 12, true, true,"male",true,false);
-            Victor victor2 = new Victor("victaur", "humain vrm pas gentil", 14, true, true, "male", true, false);
-            Victor victor3 = new Victor("vyctor", "humain pas", 11, true, true, "male", true, false);
 
-            Console.WriteLine(victor1.AffichageVictor());
-            Console.WriteLine(victor2.AffichageVictor());
-            Console.WriteLine(victor3.AffichageVictor());
+            
+            Victor[] mesVictors = new Victor[10];
+            Console.ForegroundColor = ConsoleColor.White;
+            string reponse = "";
+            Console.WriteLine("Géneration de vos Victor........");
+            Thread.Sleep(1000);
+            for (uint i = 0; i < 10; i++)
+            {
+                Victor victor = new Victor("victor " + i, "humain", 2 + i, false, false, "male", false, false);
+                mesVictors[i] = victor;
+                Console.WriteLine(mesVictors[i].AffichageVictor());
+                Console.WriteLine("---------------------------");
+            }
+            actionsVictor(ref mesVictors, "vacciner","vaccinage", 1);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            actionsVictor(ref mesVictors, "steriliser", "sterilisation", 2);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            actionsVictor(ref mesVictors, "dresser", "dressage", 3);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            actionsVictor(ref mesVictors, "pucer", "placement de la puce", 4);
+
+
+
+            for (uint i = 0; i < 10; i++)
+            {
+                Console.WriteLine(mesVictors[i].AffichageVictor());
+                Console.WriteLine("---------------------------");
+            }
+
+        }
+        static void actionsVictor(ref Victor[] mesVictors,string actions,string actions2,int numeroAction)
+        {
+            ConsoleKeyInfo cki;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Voulez vous "+ actions + " ce Victor ? o = oui / autre = non");
+                Console.WriteLine(mesVictors[i].AffichageVictor());
+                cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.O)
+                {
+                    
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(actions2 + " de votre victor en cours");
+                    for (int y = 0; y < 10; y++)
+                    {
+                        Console.Write(".");
+                        Thread.Sleep(10);
+                    }
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if (numeroAction == 1)
+                    {
+                        
+                        mesVictors[i].Vaccin = true;
+                    }
+                    else if (numeroAction == 2)
+                    {
+                        mesVictors[i].Sterilisation = true;
+                    }
+                    else if(numeroAction == 3)
+                    {
+                        mesVictors[i].Dresser = true;
+                    }
+                    else
+                    {
+                        mesVictors[i].Puce = true;
+                    }
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine();
+                    
+                }
+                else
+                {
+                    
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ce victor ne sera pas " + actions);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("---------------------------");
+
+
+                }
+            }
         }
     }
 }
--- Alexandre_exercises1_Classe/exercises1_Classe/Victor.cs
+++ Alexandre_exercises1_Classe/exercises1_Classe/Victor.cs
@@ -11,15 +11,43 @@ namespace exercises1_Classe
         // attributs privés par convention, leur nom commence par '_'
         private string _nom;
         private string _race;
-        private int _age;
+        private uint _age;
         private bool _enOrdreDeVaccin;
         private bool _puce;
         private string _genre;
         private bool _sterilise;
         private bool _dresse;
 
+        
 
-        public Victor(string nom, string race, int age, bool enOrdreDeVaccin, bool puce, string genre, bool sterilise, bool dresse)
+        public uint Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public bool Vaccin
+        {
+            get { return _enOrdreDeVaccin; }
+            set { _enOrdreDeVaccin = value; }
+        }
+        public bool Sterilisation
+        {
+            get { return _sterilise; }
+            set { _sterilise = value; }
+        }
+        public bool Dresser
+        {
+            get { return _dresse; }
+            set { _dresse = value; }
+        }
+        public bool Puce
+        {
+            get { return _puce; }
+            set { _puce = value; }
+        }
+
+
+        public Victor(string nom, string race, uint age, bool enOrdreDeVaccin, bool puce, string genre, bool sterilise, bool dresse)
         {
             _nom = nom;
             _race = race;
@@ -36,16 +64,6 @@ namespace exercises1_Classe
             string chaine = "Nom : " + _nom + "// Race : " + _race + "// Age : "+ _age +" // Vaccin : "+ _enOrdreDeVaccin +" // Puce : "+ _puce +" // Genre : "+ _genre +" // Sterilisé : "+ _sterilise +" // dresse : " + _dresse;
             return chaine;
         }
-        public void Promener() {
-            Console.WriteLine("Votre victor a été promener");
-        }
-        public void Vacciner()
-        {
-            Console.WriteLine("Votre victor a été vacciner");
-        }
-        public void Dresser()
-        {
-            Console.WriteLine("Votre victor a été dresser");
-        }
+
     }
 }
