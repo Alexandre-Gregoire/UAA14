a98bb79b4098f0f924cc120b22ae9740ef44c543
Grégoire Alexandre <210278@CCM-Asty-Moulin.local>
2023-09-26 14:29:18 +0200

    interro

---
+++ POO/I2P623_Gregoire/I2P623_Gregoire/FeuDeSignalisation.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P623_Gregoire
+{
+    internal class FeuDeSignalisation
+    {
+		private int _couleur = 1;
+		private string _identifiant;
+
+
+		public int Couleur
+        {
+			get { return _couleur; }
+			set { _couleur = value; }
+		}
+
+		public string Identifiant
+        {
+			get { return _identifiant; }
+			set { _identifiant = value; }
+		}
+
+        public FeuDeSignalisation(string identifiant, int couleur)
+        {
+			_couleur = couleur;
+            _identifiant = identifiant;
+        }
+
+        public int change(int couleur) { 
+			if (couleur == 3)
+			{
+                couleur = 1;
+            }
+			else
+			{
+                couleur++;
+			}
+			
+			
+			return couleur;
+		}
+		public bool Clignote(bool onOff, string identifiant)
+		{
+			if (onOff)
+			{
+				Console.WriteLine("le feu numero : " + identifiant + "est allumé");
+				onOff = false;
+			}
+			else
+			{
+                Console.WriteLine("le feu numero : " + identifiant + "est éteint");
+				onOff = true;
+            }
+			return onOff;
+        }
+
+		public string AfficheEtat(string identifiant, int couleur)
+		{
+			string reponse;
+			if (couleur == 1)
+			{
+                reponse = "le feux numero : " + identifiant + " est : rouge";
+            }
+			else if (couleur == 2)
+			{
+                reponse = "le feux numero : " + identifiant + " est : orange";
+            }
+			else
+			{
+                reponse = "le feux numero : " + identifiant + " est : vert";
+            }
+			
+
+			return reponse;
+		}
+
+    }
+}
+
+
+
+/*
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
---
+++ POO/I2P623_Gregoire/I2P623_Gregoire/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using I2P623_Gregoire;
+
+FeuDeSignalisation feux1 = new FeuDeSignalisation("1001",1);
+FeuDeSignalisation feux2 = new FeuDeSignalisation("007", 1);
+
+string reponse;
+bool onOff = true;
+
+Console.WriteLine("Etat des feux :\n------------");
+reponse = feux1.AfficheEtat(feux1.Identifiant,feux1.Couleur);
+Console.WriteLine(reponse);
+reponse = feux2.AfficheEtat(feux2.Identifiant, feux2.Couleur);
+Console.WriteLine(reponse);
+Console.WriteLine("Changement d'état :\n------------");
+for (int i = 0; i < 5; i++)
+{
+    feux1.Couleur = feux1.change(feux1.Couleur);
+    reponse = feux1.AfficheEtat(feux1.Identifiant, feux1.Couleur);
+    Console.WriteLine(reponse);
+}
+Console.WriteLine("faire passer le 007 à orange : ");
+feux1.Couleur = 2;
+reponse = feux1.AfficheEtat(feux1.Identifiant, feux1.Couleur);
+Console.WriteLine(reponse);
+
+Console.WriteLine("Feu clignotant : \n------------");
+for (int i = 0; i < 7; i++)
+{
+    onOff = feux2.Clignote(onOff, feux2.Identifiant);
+    feux1.Couleur = feux2.change(feux2.Couleur);
+
+
+}
+
+
