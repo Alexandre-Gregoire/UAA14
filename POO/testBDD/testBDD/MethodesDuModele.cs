b421bdbfcda0a581c55018f8f316cb6c7ae8aeaa
Grégoire Alexandre <210278@CCM-Asty-Moulin.local>
2024-01-23 15:34:46 +0100

    test bdd

---
+++ POO/testBDD/testBDD/MethodesDuModele.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testBDD
+{
+    public struct MethodesDuModele
+    {
+        public string DefinirCheminBD() // détermine la chaîne de connexion
+        {
+            return "server=10.10.51.98;database=immo;port=3306;User Id=alexandre6t;password=root";
+        }
+        public bool ChercheUtilisateurs(out DataSet infos)
+        {
+            bool ok = false;
+            MySqlConnection maConnexion = new MySqlConnection(DefinirCheminBD());
+            string query = "SELECT * FROM utilisateurs WHERE id=" + "1" + ";";
+            infos = new DataSet();
+
+            try
+            {
+                maConnexion.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnexion);
+                da.Fill(infos, "mesDonnees");
+                if (infos.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+                maConnexion.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            return ok;
+
+        }
+        public string AfficheDataUsers(DataSet donnees)
+        {
+            string infos = "";
+            for (int i = 0; i < donnees.Tables[0].Rows.Count; i++)
+            {
+                infos += donnees.Tables[0].Rows[i]["nomUser"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["prenomUser"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["loginUser"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["passWordUser"].ToString() + " | " + "\n";
+            }
+            return infos;
+        }
+    }
+}
---
+++ POO/testBDD/testBDD/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+namespace testBDD
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] donnees = new string[5];
+            DataSet immoUsers;
+            MethodesDuModele MonModel = new MethodesDuModele();
+            Console.WriteLine("Liste des utilisateurs");
+            Console.WriteLine("----------------------");
+
+            
+            if (MonModel.ChercheUtilisateurs(out immoUsers))
+            {
+                string listeUser = MonModel.AfficheDataUsers(immoUsers);
+                Console.WriteLine(listeUser);
+            }
+            else
+            {
+                Console.WriteLine("Pb lors de la lecture des données");
+            }
+        }
+        
+    }
+}
