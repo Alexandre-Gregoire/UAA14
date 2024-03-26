b5b5d4e5a376cc0392bd591aaaeb58364656f0d0
Grégoire Alexandre <210278@CCM-Asty-Moulin.local>
2024-03-26 14:33:39 +0100

    UWU

--- POO/6TI_ACT6_2_bis_GregoireA/6TI_ACT6_2_bis_GregoireA/Program.cs
+++ POO/6TI_ACT6_2_bis_GregoireA/6TI_ACT6_2_bis_GregoireA/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            
+            string reponse;
+            Joueur player = new Joueur(50, "lodiren");
+            Recette[] recettes = new Recette[25];
+            Console.WriteLine("Hello et bienvenue dans ce bar Vous posseder : "+ player.Argent.ToString() +"$\nque desirez vous faire ? : \n\n1.Créer un cocktail\n2.Voir la liste des recettes\n3.Voir les stocks de diferent ingredients\n4.Aller chercher une bouteille dans la cave\n5.\n");
+
+            reponse = Console.ReadLine();
+            switch (reponse)
+            {
+                case "1":
+
+                    Console.WriteLine("quel cocktail voulez vous faire ?");
+                    for (int i = 0; i < recettes.Length; i++)
+                    {
+                        if (recettes[i] != null)
+                        {
+                            Console.WriteLine(i.ToString() + ". " + recettes[i].Nom);
+                        }
+                        else
+                        {
+                            Console.WriteLine((i+1).ToString() + ". aucune recette atribuée");
+                        }
+                        
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+
+
+
         }
     }
 }
---
+++ POO/6TI_ACT6_2_bis_GregoireA/6TI_ACT6_2_bis_GregoireA/Recette.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TI_ACT6_2_bis_GregoireA
+{
+    internal class Recette
+    {
+		private string _nom;
+		private int[] _quantite;
+		private Liquide[] _type;
+
+		public Liquide[] Type
+		{
+			get { return _type; }
+			set { _type = value; }
+		}
+
+		public int[] Quantite
+		{
+			get { return _quantite; }
+			set { _quantite = value; }
+		}
+
+		public string Nom
+		{
+			get { return _nom; }
+			set { _nom = value; }
+		}
+
+		public Recette(string nom, int[] quantite,Liquide[] type)
+		{
+			_nom = nom;
+			_quantite = quantite;
+			_type = type;
+		}
+
+	}
+}
